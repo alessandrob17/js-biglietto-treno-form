732b430bff0f6191bd6e48fb7e9ce9db6d3774b2
alessandrob17 <a.baldipardi@gmail.com>
2023-02-01 16:58:47 +0100

    FEAT input and small css

---
+++ app.js
@@ -0,0 +1,19 @@
+var distanza = document.getElementById("distanza");
+var discount = document.getElementById("discount");
+var buttonGenerate = document.getElementById("button-generate");
+var result = document.getElementById("result");
+
+buttonGenerate.addEventListener("click", function() {
+  var distanzaValue = distanza.value;
+  var discountValue = discount.value;
+  var PREZZO_PER_CHILOMETRO = 0.21;
+  var prezzoFinale = distanzaValue * PREZZO_PER_CHILOMETRO;
+
+  if (discountValue === "minorenne") {
+    prezzoFinale = prezzoFinale * (1 - 0.2);
+  } else if (discountValue === "over") {
+    prezzoFinale = prezzoFinale * (1 - 0.4);
+  }
+
+  result.innerHTML = "Costo biglietto " + prezzoFinale.toFixed(2) + " €.";
+});
